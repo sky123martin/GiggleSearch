4dcc9bf198ee7ecb7f04745599b0620504ab3d7f
sky123martin <skylar.martin@colorado.edu>
2019-06-19 16:39:04 -0600

    Added pages and nav bar at the bottom of the results

--- app/routes.py
+++ app/routes.py
@@ -24,11 +24,12 @@ def index():
 def search():
     form = SearchForm()
     if form.validate_on_submit():
-        return redirect("/result/{}/{}:{}-{}".format(form.dataSource.data, form.region.data, form.lowerBound.data, form.upperBound.data))
+        return redirect("/result/{}/{}:{}-{}?result:1-10".format(form.dataSource.data, form.region.data, form.lowerBound.data, form.upperBound.data))
     return render_template('search.html', title='Region of Interest:', form=form)
 
 @app.route("/result/<source>/<region>:<lowerBound>-<upperBound>", methods=['GET', 'POST'])
-def result(source, region, lowerBound, upperBound, sortby = None):
+@app.route("/result/<source>/<region>:<lowerBound>-<upperBound>/pg:<page>", methods=['GET', 'POST'])
+def result(source, region, lowerBound, upperBound, page=None):
     # Definition of forms:
     filterForm = FilterResultsForm()
     searchForm = SearchForm()
@@ -40,20 +41,28 @@ def result(source, region, lowerBound, upperBound, sortby = None):
     print("INITIATING SEARCH")
     start_total = time.time()
 
-    start_task = time.time()
     overlap = getStixData(source, region, lowerBound, upperBound)
-    end_task = time.time()
-    print("####")
-    print("OVERLAPPING REGIONS FOUND AND RESULTS PARSED ({} seconds)".format(end_task - start_task))
 
+    start_task = time.time()
     if filterForm.validate_on_submit():
-        start_task = time.time()
         overlap = sorted(overlap,reverse = not filterForm.Ascending.data, key=lambda result: overlap[int(filterForm.sortBy.data)]) 
-        end_task = time.time()
-        print("#####")
-        print("FILTERING FOR SPECIFIED CONSTRAINTS ({} seconds)".format(end_task - start_task))
+    else:
+        overlap = sorted(overlap,reverse = not filterForm.Ascending.data, key=lambda result: overlap[1])
+    end_task = time.time()
+    print("#####")
+    print("FILTERING FOR SPECIFIED CONSTRAINTS ({} seconds)".format(end_task - start_task))
 
-    currentpage = overlap[0:10]
+    # determining results displayed on current page
+    if page == None:
+        page = 1
+        currentpage = overlap[0:10]
+    else:
+        page = int(page)
+        currentpage = overlap[(page*10-10):(page*10)]
+
+
+    # make array of links for pages
+
 
     start_task = time.time()
     currentpage = getDataInfo(currentpage, source) #generic function to handle data information gathering
@@ -65,13 +74,18 @@ def result(source, region, lowerBound, upperBound, sortby = None):
     print("##########")
     print("TOTAL SEARCH TIME ELAPSED {}".format(end_total - start_total))
     print("####################")
-    print("RENDERING")
+    print("RENDERING RESULTS {}-{}".format(page*10-10,page*10))
     print("########################################")
-    return render_template('result.html', searchForm=searchForm, form = filterForm, results = currentpage, searchtime = end_total - start_total, numresults = len(overlap), source = source, region = region, lowerBound = lowerBound, upperBound = upperBound)
+    return render_template('result.html', page = page, searchForm=searchForm, form = filterForm, results = currentpage, searchtime = end_total - start_total, numresults = len(overlap), source = source, region = region, lowerBound = lowerBound, upperBound = upperBound)
 
 def getStixData(source, region, lowerBound, upperBound):
+    start_task = time.time()
     url = 'https://stix.colorado.edu/{}?region={}:{}-{}'.format(source, region, lowerBound, upperBound)
     request = requests.get(url)
+    print("##")
+    print("STIX REQUEST + RETURN ({} seconds)".format(time.time() - start_task))
+    
+    start_task = time.time()
     soup = bs(request.text,"lxml")
     text = soup.text.split('\n') 
     results = []
@@ -88,6 +102,8 @@ def getStixData(source, region, lowerBound, upperBound):
             temp[0] = temp[0].split(".bed.gz")[0]
             temp.append(round(((temp[2]/temp[1])*100.0),10))
             results.append(temp)
+    print("####")
+    print("STIX OVERLAPPING REGIONS PARSED ({} seconds)".format(time.time() - start_task))
     return results
 
 def getDataInfo(data, source):
