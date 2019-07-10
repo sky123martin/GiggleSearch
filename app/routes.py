1995281737c70f88c3d31b396f155ffd50caa096
sky123martin <skylar.martin@colorado.edu>
2019-07-10 14:41:01 -0600

    Optimized large and small scale information gathering using pandas data frame

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, flash, redirect
 from app import app
 from app.forms import SearchForm, FilterResultsForm, Search
 from bs4 import BeautifulSoup as bs
+import pandas as pd 
+import numpy as np 
+from multiprocessing.dummy import Pool as ThreadPool 
+import multiprocessing
 from flaskext.mysql import MySQL
 import time
+import math
 import requests
 import re
 
@@ -31,9 +36,7 @@ def parseSearch(Input):
         print("Match Found:",result.group(0))
         str = result.group(0)
         numbers = [ int(s) for s in str.split() if s.isdigit()]
-        print(numbers)
         word_list = str.split()
-        print(word_list[-1])
         return redirect("/result/{}/chr{}:{}-{}".format(word_list[-1].lower(), numbers[2], numbers[0], numbers[1]))
     else:
         print("Parser Error: No Match Found")
@@ -68,27 +71,40 @@ def result(source, region, lowerBound, upperBound, page=None, sort = None, asc =
         overlap.sort(key = lambda ele : ele[int(sort)], reverse = bool(asc))
     else:
         overlap.sort(key = lambda ele : ele[2], reverse = 1)
-        print(overlap[0:5])
 
     end_task = time.time()
     print("#####")
     print("FILTERING FOR SPECIFIED CONSTRAINTS ({} seconds)".format(end_task - start_task))
-
+    #Pagination
     # determining results displayed on current page
     if page == None:
         page = 1
-  
+    else:
+        page = int(page)
 
-    page = int(page)
+    maxpage = int(math.ceil(len(overlap)/10.0))
+    print(len(overlap),len(overlap)/10,math.ceil(len(overlap)/10))
+    if page < 7 or maxpage < 10:
+        if maxpage < 10:
+            pageNums = list(range(1, maxpage + 1))
+        else:
+            pageNums = list(range(1, 11))
+    else:
+        if maxpage < (page + 4):
+            pageNums = list(range(page-5, maxpage + 1))
+        else:
+            pageNums = list(range(page-5, page + 5))
     currentpage = overlap[(page*10-10):(page*10)]
-
+    print("Pagination",pageNums, page, maxpage)
 
     # make array of links for pages
     start_task = time.time()
-    currentpage = getDataInfo(currentpage, source) #generic function to handle data information gathering
+    overlap = getDataInfo(overlap, source) #generic function to handle data information gathering
     end_task = time.time()
     print("########")
     print("DATA SOURCE INFOMATION COLLECTED ({} seconds)".format(end_task - start_task))
+     
+    currentpage = overlap[(page*10-10):(page*10)]
 
     end_total = time.time()
     print("##########")
@@ -96,7 +112,7 @@ def result(source, region, lowerBound, upperBound, page=None, sort = None, asc =
     print("####################")
     print("RENDERING RESULTS {}-{}".format(page*10-10,page*10))
     print("########################################")
-    return render_template('result.html', page = page, form = form, filterform = filterForm, results = currentpage, searchtime = end_total - start_total, numresults = len(overlap), source = source, region = region, lowerBound = lowerBound, upperBound = upperBound)
+    return render_template('result.html', page = page, form = form, filterform = filterForm, pageNums=pageNums, results = currentpage, searchtime = end_total - start_total, numresults = len(overlap), source = source, region = region, lowerBound = lowerBound, upperBound = upperBound)
 
 def getStixData(source, region, lowerBound, upperBound):
     start_task = time.time()
@@ -120,8 +136,9 @@ def getStixData(source, region, lowerBound, upperBound):
                 temp[0] = split[1]
 
             temp[0] = temp[0].split(".bed.gz")[0]
-            temp.append(round(((temp[2]/temp[1])*100.0),10))
-            results.append(temp)
+            # temp.append(round(((temp[2]/temp[1])*100.0),10))
+            if temp[0][-4:] != "Link":
+                results.append(temp)
     print("####")
     print("STIX OVERLAPPING REGIONS PARSED ({} seconds)".format(time.time() - start_task))
     return results
@@ -133,22 +150,69 @@ def getDataInfo(data, source):
         return data
 
 def getUCSCData(results):
+    # Dataformat: [tablename, regionsize, overlap, shortname, longname, html, description]
+    # 1. Query all at once and sort results
+    print(results[1])
+    start_task = time.time()
     conn = mysql.connect()
     cursor = conn.cursor()
-    for temp in results:
-        query = "SELECT shortLabel, longLabel, html from hg19.trackDb where tableName = '{}'".format(temp[0])
+    query = "SELECT tableName, shortLabel, longLabel, html from hg19.trackDb order by tableName"
+    cursor.execute(query)
+    orderedlist = []
+
+    for row in cursor:
+        orderedlist.append(row)
+    orderedlist = pd.DataFrame(orderedlist, columns=["tableName", "shortLabel", "longLabel", "html"])
+    dfresults = pd.DataFrame(results, columns=["tableName","regionsize","overlap"])
+    result = pd.merge(dfresults, orderedlist, how='left', on='tableName')
+    result = result.fillna("")
+    result = result.values.tolist()
+
+    for data in result:
+        if data[5] != "":
+            data.append(getUCSCdescription(data[5]))
+        else:
+            data.append("")
+
+    end_task = time.time()
+    print("One Query with Pandas:", end_task - start_task)
+
+    ## 2. With multithreading on individual queries
+    # start_task = time.time()
+    # pool = ThreadPool(100) 
+    # end = pool.map(queryUCSC, results)
+    # end_task = time.time()
+    # print("Multithreading:", end_task - start_task)
+    # print(results)
+
+    ### 3. Without multi threading on individual queries
+    # start_task = time.time()
+    # conn = mysql.connect()
+    # cursor = conn.cursor()
+    # final = []
+    # for temp in results:
+    #     final.append(queryUCSC(temp))
+    # end_task = time.time()
+
+    # print("Normal:",end_task-start_task)
+    return result
+ 
+def queryUCSC(tablename):
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        query = "SELECT shortLabel, longLabel, html from hg19.trackDb where tableName = '{}'".format(tablename[0])
         cursor.execute(query)
         data = cursor.fetchone()
         if data != None:
-            temp[0] = data[0] + ":  " + data[1]
+            tablename[0] = data[0] + ":  " + data[1]
             if data[2] == "":
-               temp.append("No further information on dataset found.") 
+               tablename.append("No further information on dataset found.") 
             else:
-                temp.append(data[2])
-                temp.append(getUCSCdescription(data[2]))
+                tablename.append(data[2])
+                tablename.append(getUCSCdescription(data[2]))
         else:
-            temp.append("No further information on dataset found.")
-    return results
+           tablename.append("No further information on dataset found.")
+        return tablename
 
 def getUCSCdescription(html):
     # Format of descriptions <H3>Description</H3> <P> ... <P> <h2>Description</h2> <p>
